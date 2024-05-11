3c58d9d13945bb84d6e986b81643d5b67a9272d3
Mohamed Hamdy <113174295+Hamdy005@users.noreply.github.com>
2024-05-11 23:39:50 +0300

    Update Script.js

--- JS/Script.js
+++ JS/Script.js
@@ -1,116 +1,97 @@
 class Quiz {
 
-  #score = 0;
-  #currQuestionNo = 0;
-  #currOptions = document.getElementsByClassName("choice");
-  #questions = [ 
-
-    {question: "What kind of language is Javascript?",
-    options: ["Object-oriented"," Object-based"," Procedural"," None of the above"],
-    answer: 1},
-
-    {question: "How do you define a variable in Javascript?",
-    options: [" var", " let", "Both", " None"],
-    answer: 2},
-
-    {question: "What methods can display data in Javascript?",
-    options: [" document.write()"," console.log()"," window.alert()"," All of the above"],
-    answer: 1}
-
-  ]
+    #score = 0;
+    #currQuestionNo = 0;
+    #currOptions = document.getElementsByClassName("choice");
+    #questions = [ 
   
-  #scoreLabel = document.getElementById("curr-score");
-  #quizHdr = document.getElementById("right-cont-hdr");
-  #questionPar = document.getElementById("question");
+      {question: "What kind of language is Javascript?",
+      options: ["Object-oriented"," Object-based"," Procedural"," None of the above"],
+      answer: 1},
   
-
-  constructor() {
-
-    this.guiDisplay();
+      {question: "How do you define a variable in Javascript?",
+      options: [" var", " let", "Both", " None"],
+      answer: 2},
   
-  }
-
-  showQuestion(q) {
-    // q parameter for making obkject
-    console.log(
-      "%c" + q.question,
-      "color: #4CAF50; font-size: 18px; font-weight: bold"
-    ); // Display the question
-    q.options.forEach(
-      (
-        option,
-        index // for each loop to iterate over every option
-      ) =>
-        console.log(
-          `  - %c${index + 1}. ${option}`,
-          "color: #2196F3; font-size: 16px;"
-        ) // beacuse the array start with index zero we +1 to index and show questions
-    );
-  }
-
-  guiDisplay() {   
-
-    if(this.#currQuestionNo >= this.#questions.length) {
-
-      let scoreLabel = `${this.#score} / ${this.#currQuestionNo}`;
-
-      alert(`Your answers are submitted successfully \n score: ${scoreLabel}`);
-      console.log(`%c Your final score is: ${scoreLabel}`, "color: #FF5722; font-size: 18px; font-weight: bold;")
-      return;
-
-    }
-
-    let ind = this.#currQuestionNo;
-    let currObject = this.#questions[ind];
+      {question: "What methods can display data in Javascript?",
+      options: [" document.write()"," console.log()"," window.alert()"," All of the above"],
+      answer: 1}
   
-    this.#quizHdr.innerHTML = `Question ${ind + 1}`;
-    this.#questionPar.innerHTML = currObject.question;
-
-    for(let j = 0; j < 4; ++j) {
-
-      this.#currOptions[j].innerHTML = currObject.options[j];
-      this.#currOptions[j].style.border = "3px solid black";
-
-    }
-
-    this.showQuestion(this.#questions[this.#currQuestionNo]);
-    setTimeout(() => {this.choose()}, 500);
-  }
-
-  choose() {
-
-    let choosenAnswer = prompt("Choose the correct answer");
-
-    let correct = false;
-    let correctAnswer = this.#questions[this.#currQuestionNo++].answer;
-
-    if(choosenAnswer == correctAnswer + 1) {
-
-      correct = true;
-      document.getElementById(`choice${choosenAnswer}`).style.border = "5px solid green";
-      console.log(`%c Correct`, "color: #4CAF50; font-size: 16px; font-weight: bold");
-
-    } 
+    ]
     
-    else {
-
-      document.getElementById(`choice${choosenAnswer}`).style.border = "5px solid red";
-      document.getElementById(`choice${correctAnswer + 1}`).style.border = "5px solid green";
-      console.log(`%c Incorrect. The correct answer is: ${correctAnswer}`, "color: #F44336; font-size: 16px;");
-
+    #scoreLabel = document.getElementById("curr-score");
+    #quizHdr = document.getElementById("right-cont-hdr");
+    #questionPar = document.getElementById("question");
+    
+  
+    constructor() {
+      this.display();
     }
+  
+    display() {   
+  
+        if(this.#currQuestionNo >= this.#questions.length) {
 
-    let scoreLabel = `${this.#score} / ${this.#currQuestionNo}`;
+            let scoreLabel = `${this.#score} / ${this.#currQuestionNo}`;
+        
+            alert(`Your answers are submitted successfully \n score: ${scoreLabel}`);
+            console.log(`%c Your final score is: ${scoreLabel}`, "color: #FF5722; font-size: 18px; font-weight: bold;")
+            return;
+        
+        }
+      
+      let ind = this.#currQuestionNo;
+      let currObject = this.#questions[ind];
+    
+      this.#quizHdr.innerHTML = `Question ${ind + 1}`;
+      this.#questionPar.innerHTML = currObject.question;
+      console.log(`%c + ${currObject.question}`, "color: #4CAF50; font-size: 18px; font-weight: bold")
+  
+      for(let j = 0; j < 4; ++j) {
+  
+        this.#currOptions[j].innerHTML = currObject.options[j];
+        this.#currOptions[j].style.border = "3px solid black";
+        console.log(`%c ${j + 1}.${currObject.options[j]}`, "color: #2196F3; font-size: 16px;")
+  
+      }
+      setTimeout(() => {this.choose()}, 500);
+    }
+  
+    choose() {
+  
+      let choosenAnswer = prompt("Choose the correct answer");
+  
+      let correct = false;
+      let correctAnswer = this.#questions[this.#currQuestionNo].answer;
+  
+      if(choosenAnswer == correctAnswer + 1) {
+  
+        correct = true;
+        document.getElementById(`choice${choosenAnswer}`).style.border = "5px solid green";
+        console.log(`%c Correct`, "color: #4CAF50; font-size: 16px; font-weight: bold");
+  
+      } 
+      
+      else {
+  
+        document.getElementById(`choice${choosenAnswer}`).style.border = "5px solid red";
+        document.getElementById(`choice${correctAnswer + 1}`).style.border = "5px solid green";
+        console.log(`%c Incorrect. The correct answer is: ${correctAnswer}`, "color: #F44336; font-size: 16px;");
+  
+      }
+
     this.#score += correct;
-    this.#scoreLabel.innerHTML = scoreLabel
+
+    let scoreLabel = `${this.#score} / ${++this.#currQuestionNo}`;
+    this.#scoreLabel.innerHTML = scoreLabel;
 
     if(this.#currQuestionNo < this.#questions.length)
       console.log(`%c Your current score is: ${scoreLabel}`, "color: #795548; font-size: 16px; font-weight: bold;");
     
-    setTimeout(() => {this.guiDisplay()}, 500);
-    
-  }
-
-};
-
-new Quiz();
+    setTimeout(() => {this.display()}, 500);
+      
+    }
+  
+  };
+  
+  new Quiz();
